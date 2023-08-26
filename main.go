635bf1152cf88c0b99684df3f6411aa0f018230b
ZSC714725 <zang199193@163.com>
2023-08-26 22:30:32 +0800

    add jessibuca and mpegts

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.StaticFS("/js", http.Dir("./js"))
+	router.StaticFS("/jpg", http.Dir("./jpg"))
+	router.StaticFS("/css", http.Dir("./css"))
+	router.GET("/jessibuca.html", HandleJessibuca)
+	router.GET("/mpegts.html", HandleMpegts)
+	router.GET("/favicon.ico", HandleFavicon)
+
+	router.Run(":8080")
+}
+
+func HandleJessibuca(c *gin.Context) {
+	c.HTML(200, "jessibuca.html", nil)
+}
+
+func HandleFavicon(c *gin.Context) {
+	c.Status(http.StatusOK)
+}
+
+func HandleMpegts(c *gin.Context) {
+	c.HTML(200, "mpegts.html", nil)
+}
